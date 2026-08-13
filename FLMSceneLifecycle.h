2948c55bf3343271cfc92e21ef93e4e1af842f33
farawayingusa-glitch <farawayingusa@gmail.com>
2026-08-13 14:45:30 +0800

    Add preference plist files

---
+++ FLMSceneLifecycle.h
@@ -0,0 +1,12 @@
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+FOUNDATION_EXPORT NSNotificationName const FLMProtectedSceneDidDisappearNotification;
+
+void FLMProtectScene(id scene, id _Nullable sceneHandle);
+void FLMClearProtectedScene(id _Nullable scene);
+BOOL FLMObjectMatchesProtectedScene(id _Nullable object);
+BOOL FLMProtectedSceneIsAlive(void);
+
+NS_ASSUME_NONNULL_END
